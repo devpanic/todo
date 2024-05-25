b352f14c010400a1b1dfa5e33758490a18823cb9
devpanic <yeon.dev.8@gmail.com>
2024-05-25 23:41:02 +0900

    Initial Commit

---
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -0,0 +1,30 @@
+package com.example.accessingdatamysql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    private String name;
+
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/accessingdatamysql/UserDTO.java
@@ -0,0 +1,11 @@
+package com.example.accessingdatamysql;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private String name;
+    private String email;
+}
